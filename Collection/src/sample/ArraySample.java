a075ced9f9df91c73cad9c1c12acef54a2c1b555
dir-FY24muramatsukyousuke <KyosukeMuramatsu(@Dirbato-1315x>
2024-04-10 10:36:48 +0900

    3回目のコミット

--- Collection/src/sample/ArraySample.java
+++ Collection/src/sample/ArraySample.java
@@ -2,9 +2,15 @@ package sample;
 
 public class ArraySample {
 
-    public static void main(String[] args) {
-        // TODO 自動生成されたメソッド・スタブ
-
+    public static void main(String[]args) {
+        var number =new int[5];
+    
+         
+        for(var i = 0; i < number.length; i++) {
+            number [i] =  i * 2;
+    }            
+        for(var i=0; i < number.length; i++) {
+            System.out.println(number[i]);
     }
-
+  }
 }
